7697bdd799225994df04b300d89213244c30a48f
나재윤 <jyr1492@gmail.com>
2021-09-01 15:55:05 +0900

    Add project files.

---
+++ ThePhotorealistic/D3DUtil.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "lib/DirectXTex/DirectXTex.lib")
+#pragma comment(lib, "dxguid.lib")
+
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+#include <wrl.h>
+
+using Microsoft::WRL::ComPtr;
---
+++ ThePhotorealistic/ThePhotorealistic.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "resource.h"
+#include "D3DUtil.h"
+#include "GameTimer.h"
+#include <string>
+
+class ThePhotorealistic
+{
+public:
+	ThePhotorealistic(HINSTANCE hInstance);
+	virtual ~ThePhotorealistic();
+
+	HINSTANCE AppInst()const;
+	HWND MainWnd()const;
+	float AspectRatio()const;
+
+	int Run();
+
+	// Framework methods.  Derived client class overrides these methods to 
+	// implement specific application requirements.
+
+	virtual bool Init();
+	virtual void OnResize();
+	virtual void UpdateScene(float dt);
+	virtual void DrawScene();
+	virtual LRESULT MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+	// Convenience overrides for handling mouse input.
+	virtual void OnMouseDown(WPARAM btnState, int x, int y) { }
+	virtual void OnMouseUp(WPARAM btnState, int x, int y) { }
+	virtual void OnMouseMove(WPARAM btnState, int x, int y) { }
+
+private:
+	bool InitMainWindow();
+	bool InitDirect3D();
+
+	void CalculateFrameStats();
+
+private:
+
+	HINSTANCE mhAppInst;
+	HWND mhMainWnd;
+	bool mAppPaused;
+	bool mMinimized;
+	bool mMaximized;
+	bool mResizing;
+	UINT m4xMsaaQuality;
+
+	GameTimer mTimer;
+
+	ComPtr<ID3D11Device> md3dDevice;
+	ComPtr<ID3D11DeviceContext> md3dImmediateContext;
+	ComPtr<IDXGISwapChain> mSwapChain;
+	ComPtr<ID3D11Texture2D> mDepthStencilBuffer;
+	ComPtr<ID3D11RenderTargetView> mRenderTargetView;
+	ComPtr<ID3D11DepthStencilView> mDepthStencilView;
+	D3D11_VIEWPORT mScreenViewport;
+
+	// Derived class should set these in derived constructor to customize starting values.
+	std::wstring mMainWndCaption;
+	D3D_DRIVER_TYPE md3dDriverType;
+	int mClientWidth;
+	int mClientHeight;
+	bool mEnable4xMsaa;
+};
